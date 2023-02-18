8bc61b5f58554f89d314a2bad615a1fdbcd355eb
Jakub Kozlowski <jakub.kozlowski1104@gmail.com>
2023-02-18 11:10:49 +0000

    user menu

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -4,6 +4,7 @@ import "./Navbar.scss";
 // import { Link } from "react-dom";
 const Navbar = () => {
   const [active, setActive] = useState(false);
+  const [isMenuActive, setIsMenuActive] = useState(true);
 
   const isActive = () => {
     window.scrollY > 0 ? setActive(true) : setActive(false);
@@ -16,6 +17,16 @@ const Navbar = () => {
       window.addEventListener("scroll", isActive);
     };
   }, []);
+
+  const handleMenu = () => {
+    setIsMenuActive(!isMenuActive);
+  };
+
+  const currentUser = {
+    id: 1,
+    username: "John Doe",
+    isSeller: true,
+  };
 
   return (
     <div className={active ? "navbar active" : "navbar"}>
@@ -31,8 +42,30 @@ const Navbar = () => {
           <span>Explore</span>
           <span>English</span>
           <span>Sign in</span>
-          <span>Become a Seller</span>
-          <button>Join</button>
+          {!currentUser?.isSeller && <span>Become a Seller</span>}
+          {!currentUser && <button>Join</button>}
+          {currentUser && (
+            <div className="user" onClick={handleMenu}>
+              <img
+                src="https://images.pexels.com/photos/1115697/pexels-photo-1115697.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt="user-logo"
+              />
+              <span>{currentUser?.username}</span>
+              {isMenuActive && (
+                <div className="options">
+                  {currentUser?.isSeller && (
+                    <>
+                      <span>Gigs</span>
+                      <span>Add New Gig</span>
+                    </>
+                  )}
+                  <span>Orders</span>
+                  <span>Messages</span>
+                  <span>Logout</span>
+                </div>
+              )}
+            </div>
+          )}
         </div>
       </div>
       <div>
