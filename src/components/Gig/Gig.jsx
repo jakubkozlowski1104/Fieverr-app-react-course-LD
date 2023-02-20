1029fab15efc2dafe3538bd7d6d0414c7bf1fa65
Jakub Kozlowski <jakub.kozlowski1104@gmail.com>
2023-02-20 10:41:23 +0000

    create slider

---
+++ src/components/Gig/Gig.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Gig.scss";
+import Slider from "infinite-react-carousel";
+
+const Gig = () => {
+  return (
+    <div className="gig">
+      <div className="container"></div>
+    </div>
+  );
+};
+
+export default Gig;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,13 +1,20 @@
 import React from "react";
 import Featured from "../../components/featured/Featured";
+import Slide from "../../components/slide/Slide";
 import TrustedBy from "../../components/trustedBy/TrustedBy";
 import "./Home.scss";
-
+import CatCard from "../../components/catCard/CatCard";
+import { cards } from "../../data";
 const Home = () => {
   return (
     <div className="home">
       <Featured />
       <TrustedBy />
+      <Slide slidesToShow={5} arrowsScroll={5}>
+        {cards.map((card) => (
+          <CatCard key={card.id} card={card} />
+        ))}
+      </Slide>
     </div>
   );
 };
