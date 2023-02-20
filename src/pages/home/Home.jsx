a19ee4b67c5a18aa122847976a326c18a47eddff
Jakub Kozlowski <jakub.kozlowski1104@gmail.com>
2023-02-20 12:35:56 +0000

    move code feature-video to new component FeatureVideo

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -5,6 +5,7 @@ import TrustedBy from "../../components/trustedBy/TrustedBy";
 import "./Home.scss";
 import CatCard from "../../components/catCard/CatCard";
 import { cards } from "../../data";
+import FeatureVideo from "../../components/FeatureVideo/FeatureVideo";
 const Home = () => {
   return (
     <div className="home">
@@ -15,48 +16,7 @@ const Home = () => {
           <CatCard key={card.id} card={card} />
         ))}
       </Slide>
-      <div className="features">
-        <div className="container">
-          <div className="item">
-            <h1>A whole world of freelance talent at your fingertips</h1>
-            <div className="title">
-              <img src="./img/check.png" alt="" />
-              The best for every budget
-            </div>
-            <p>
-              Find high-quality services at every price point. No hourly rates,
-              just project-based pricing.
-            </p>
-            <div className="title">
-              <img src="./img/check.png" alt="" />
-              Quality work done quickly
-            </div>
-            <p>
-              Find the right freelancer to begin working on your project within
-              minutes.
-            </p>
-            <div className="title">
-              <img src="./img/check.png" alt="" />
-              Protected payments, every time
-            </div>
-            <p>
-              Always know what youll pay upfront. Your payment isnt released
-              until you approve the work.
-            </p>
-            <div className="title">
-              <img src="./img/check.png" alt="" />
-              24/7 support
-            </div>
-            <p>
-              Find high-quality services at every price point. No hourly rates,
-              just project-based pricing.
-            </p>
-          </div>
-          <div className="item">
-            <video src="./img/video.mp4" controls />
-          </div>
-        </div>
-      </div>
+      <FeatureVideo />
     </div>
   );
 };
