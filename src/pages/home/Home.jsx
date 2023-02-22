d634037a1cfe159eebb8c9788c61a13481dd2f5c
Jakub Kozlowski <jakub.kozlowski1104@gmail.com>
2023-02-22 22:18:30 +0000

    create card slider

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,9 +4,11 @@ import Slide from "../../components/slide/Slide";
 import TrustedBy from "../../components/trustedBy/TrustedBy";
 import "./Home.scss";
 import CatCard from "../../components/catCard/CatCard";
-import { cards } from "../../data";
+import { cards, projects } from "../../data";
 import FeatureVideo from "../../components/FeatureVideo/FeatureVideo";
 import FeatureBusiness from "../../components/FeatureBusiness/FeatureBusiness";
+import ProjectCard from "../../components/ProjectCard/ProjectCard";
+
 const Home = () => {
   return (
     <div className="home">
@@ -19,6 +21,11 @@ const Home = () => {
       </Slide>
       <FeatureVideo />
       <FeatureBusiness />
+      <Slide slidesToShow={4} arrowsScroll={4}>
+        {projects.map((card) => (
+          <ProjectCard key={card.id} card={card} />
+        ))}
+      </Slide>
     </div>
   );
 };
